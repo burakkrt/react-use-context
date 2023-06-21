7caf831327ccbc9491d3889b9771a78f1d452de0
BurakKurt <krtburak@outlook.com>
2023-06-21 14:29:20 +0300

    project created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 function App() {
 
 
-  return (
-    <>
-    <h1 className="fs-4">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-2xl text-red-200">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam</h1>
+            <p className="m-20 text-green-400">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                eirmod tempor
+                invidunt ut labore
+                et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+                rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+                clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Fragment>
-    <App />
-  </Fragment>,
+    <React.Fragment>
+        <App/>
+    </React.Fragment>,
 )
