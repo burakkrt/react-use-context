a26de53faee39ff8b626a58f29dc58566d5dad29
BurakKurt <krtburak@outlook.com>
2023-06-21 14:31:51 +0300

    chanced

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
         <>
             <h1 className="text-2xl text-red-200">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                 diam</h1>
-            <p className="m-20 text-green-400">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+            <p className="m-20 text-red-700 bg-slate-300 p-2">Lorem ipsum dolor sit amet, consetetur
+                sadipscing
+                elitr,
+                sed
+                diam nonumy
                 eirmod tempor
                 invidunt ut labore
                 et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
